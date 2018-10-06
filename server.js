2b5b8ea3f3668eff5972ca58e314e7fde2d30f22
william zhu <william_zhu@hotmail.ca>
2018-10-06 00:03:58 -0400

    changed mongo link

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(express.static("public"));
 mongoose.connect("mongodb://localhost/newInfo", { useNewUrlParser: true });
 
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/";
 
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
