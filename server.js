6046e22c0a3ee6900598528b715d670a48145617
william zhu <william_zhu@hotmail.ca>
2018-10-06 00:07:44 -0400

    deleted a mongoose line

--- server.js
+++ server.js
@@ -18,9 +18,6 @@ app.use(express.json());
 // Make public a static folder
 app.use(express.static("public"));
 
-// Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/newInfo", { useNewUrlParser: true });
-
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/";
 
